def25dbb579aad5fc18c2e61b1436c2dac9afaea
KathrynDH <47955251+KathrynDH@users.noreply.github.com>
2021-06-23 14:18:27 -0400

    Update getdata-lit-income.py

--- data-scripts/getdata-lit-income.py
+++ data-scripts/getdata-lit-income.py
@@ -10,46 +10,8 @@ and save the dataframe as as CSV file
 Indicators: GNP and youth literacy rate for most recent year available
 """
 
-import requests
-import pandas as pd
+from functions import get_wb_data, clean_wb_df
 
-def get_wb_data(var, param, counrty='all'):
-    """
-    Function to get data from World Bank API
-
-    Args:
-        var (string): indicator of interest
-        param (string): parameters to send with API call
-        country (string): counrties of interest
-
-    Returns:
-        Pandas dataframe from json response data
-        
-    """
-    req = 'http://api.worldbank.org/v2/country/'+counrty+'/indicator/'+var
-    response = requests.get(req, params=param)
-    response.raise_for_status()
-    return pd.DataFrame(response.json()[1])
-
-def clean_wb_df(df, value_column):
-    """
-    Function to get clean data from World Bank API
-
-    Args:
-        df (dataframe): dataframe created from worldbank json data
-        value_column (string): name of value column to be created
-
-    Returns:
-        Pandas dataframe with counrty code as key, value column and year
-        
-    """
-    df2 = pd.DataFrame(columns=['country',value_column,value_column+'_year'])
-    for index, row in df.iterrows():
-        country = row['country']
-        country_code = country['id']
-        data = [country['value'],row['value'],row['date']]
-        df2.loc[country_code] = data
-    return df2
         
 save_filename = '../data/worldbank-lit-inc.csv' #file name to save to
 total_records = '500' #maximum results to return from API call
@@ -66,6 +28,10 @@ col_name.append('gnp')
 var.append('SE.ADT.1524.LT.ZS')
 col_name.append('youth_literacy')
 
+#Get population
+var.append('SP.POP.TOTL')
+col_name.append('population')
+
 
 # Get a dataframe for each indicator for most recent year available (mrnev=1)
 for i in range(len(var)):
@@ -76,7 +42,7 @@ for i in range(len(var)):
         'mrnev':'1'
     }
     df1 = get_wb_data(var[i],parameters)
-    df2 = clean_wb_df(df1, col_name[i])
+    df2 = clean_wb_df(df1, col_name[i], mrnev=True)
     df.append(df2)
 
 # Join the dataframes
